e993dd321bafe595a8b22f2460a4f6821fc204c7
Kyle Sangillo <kasangillo@email.neit.edu>
2021-10-29 00:05:33 -0400

    Bio3

---
+++ Bio_3/bio3.js
@@ -0,0 +1,61 @@
+
+var store1="Time"
+var store2="He was Bald"
+var store3="Silences"
+var store4="A Bank"
+var store5="An Echo"
+var store6="The Future"
+var store7="A River"
+var store8="A Hole"
+var store9="Fire"
+var store10="His Son"
+
+function answer1() {
+
+    document.getElementById("question1").innerHTML = store1;
+}
+function answer2() {
+
+    document.getElementById("question2").innerHTML = store2;
+}
+
+function answer3() {
+
+    document.getElementById("question3").innerHTML = store3;
+}
+
+function answer4() {
+
+    document.getElementById("question4").innerHTML = store4;
+}
+function answer5() {
+
+    document.getElementById("question5").innerHTML = store5;
+}
+function answer6() {
+
+    document.getElementById("question6").innerHTML = store6;
+}
+function answer7() {
+
+    document.getElementById("question7").innerHTML = store7;
+
+}
+
+function answer8() {
+
+    document.getElementById("question8").innerHTML = store8;
+
+}
+
+function answer9() {
+
+    document.getElementById("question9").innerHTML = store9;
+
+}
+
+function answer10() {
+
+    document.getElementById("question10").innerHTML = store10;
+
+}

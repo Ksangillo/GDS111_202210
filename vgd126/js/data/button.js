da19eadf13056764987fb34502b963c4f5f5978c
Sangillo <kasangillo@email.neit.edu>
2022-02-28 14:41:34 -0500

    vgd126

---
+++ vgd126/js/data/button.js
@@ -0,0 +1,29 @@
+var buttonData ={
+    info:{
+        src:'images/starthere.png'
+    },
+    states:{
+        idle:{
+            fps:15,
+            cycle:true,
+            frames:
+            [
+                {width:128,height:27, startX:0, startY:0},
+            
+            ]
+        },
+
+        hover:{
+            fps:60,
+            cycle:false,
+            frames:
+            [
+                {width:128,height:27, startX:128,startY:0}
+                
+            ]
+
+        }
+
+    }
+
+}
--- vgd126/js/gamestates/menu.js
+++ vgd126/js/gamestates/menu.js
@@ -2,15 +2,11 @@
 This file contains all of the code for the Main Menu
 ----------------------------------*/
 
-var startButton = new GameObject();
-//startButton.img.src="images/mrt.jpg"
-startButton.width=200;
-startButton.hitBoxWidth=800
-console.log(startButton.collisionPoints.right)
+var startButton = new GameObject({width:128,height:27}).makeSprite(buttonData)//turns into a sprite for a animated spritesheet for menu
 
 
 var menuBackground = new GameObject();
-menuBackground.img.src = "images/mrt.jpg"
+menuBackground.img.src = "images/menu.jpg"
 menuBackground.width=canvas.width
 menuBackground.height=canvas.height
 
@@ -22,20 +18,24 @@ gameStates[`menu`] =function(){
 		if(mouse.pressed)
 		{
 			//Changes to the game state
+			
 			gameStates.changeState(`level1`)
 		}
 
 		//Hover Effect Graffic
 		startButton.color = `yellow`
+		startButton.changeState(`hover`)
+		
 	}
 	else
 	{
 		//Default Button Graphic
 		startButton.color = `red`
+		startButton.changeState('idle')//changes back to idle
 	}
 	
 	menuBackground.drawStaticImage();
-	startButton.render()
+	startButton.play().drawSprite()//.drawsprite calls a sprite
 }
 	
 	

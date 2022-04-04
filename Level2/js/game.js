64f0fc40649784d44d8545c1f122f36b037b0c28
Kyle <kasangillo@email.neit.edu>
2022-04-04 18:14:27 -0400

    level2

--- Level2/js/game.js
+++ Level2/js/game.js
@@ -5,6 +5,7 @@ var interval = 1000/60;
 var player1; 
 var prevY1;
 var prevY2;
+var prevyX;
 
 canvas = document.getElementById("canvas");
 	context = canvas.getContext("2d");
@@ -16,6 +17,8 @@ player1.width = 30;
 player1.height= 160;
 
 ball = new GameObject();
+ball.vx = 4;
+ball.vy = 0;
 
 LTblock = new GameObject(canvas.width , canvas.height, 30, 50);
 LTblock.x= 60;
@@ -63,28 +66,57 @@ function animate()//
 	{
 		prevY2 = player1.y ;
 	}
+	if(ball.hitTestObject(player1))
+	{
+		ball.vx = -ball.vx;
+		ball.vx= 1 + ball.vx;
+		ball.color="purple";
+	}
+
 	
-	
-
-
-
-
-
-
-
-
-
-
 
 
 
 	
+	//---------------------BALL---------------------------
+	ball.move();
+	//--------------Bounce of Right----------------------
+	if(ball.x > canvas.width - ball.width/2 ) 
+	{
+		ball.x = canvas.width - ball.width/2
+		ball.vx = -ball.vx;//reverses the direction
+		ball.vx= 1 + ball.vx;//ball increases speed x
+		ball.color="red";//changes color cause of var ball
+		
+	}
+	//-------------Bounce of Left-----------------------
+	 if (ball.x < 0 + ball.width/2 )
+	{
+		ball.color="black";
+		ball.vx = -ball.vx;
+		ball.vx= 1 + ball.vx;//ball increases speed y
+		
+	}
+	if (ball.y > canvas.height - ball.height/2)
+	{
+
+		ball.color="yellow";
+		ball.vy = -ball.vy
+		ball.vy= 1 + ball.vy;
+	}
+	 if (ball.y < 0 + ball.height/2)
+	{
+		ball.color="cyan";
+		ball.vy = -ball.vy
+		ball.vy= 1 + ball.vy;
+	}
+
+
+	//---------------------------------------------------
 	
-
-
+	ball.drawCircle();
 	player1.drawRect();
-	LTblock.drawRect();
-	LBblock.drawRect();
+	
 	
 	
 }//
